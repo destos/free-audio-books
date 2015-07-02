f5f25fdfcb4e53c5deac1f7e1f1d445bc0653d7f
Patrick Forringer <patrick@forringer.com>
2015-07-01 21:51:31 -0500

    missed doc files and book migration

---
+++ free_audio_books/books/migrations/0002_auto_20150226_1858.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('books', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='category',
+            options={'verbose_name': 'Category', 'verbose_name_plural': 'Categories'},
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='will_scrape',
+            field=models.BooleanField(default=True),
+            preserve_default=True,
+        ),
+    ]
